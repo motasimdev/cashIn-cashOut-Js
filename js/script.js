02d14172ae0056a0a8abb90d3075ae7e95f31954
motasimdev <themotasimdev@gmail.com>
2025-06-05 14:42:31 +0600

    aa

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ loginBtn.addEventListener("click", ()=>{
     let NmbrValue = mobileNumber.value 
     let PinValue = mobilePin.value 
     if (NmbrValue == "01625419860" && PinValue == 2222) {
-        console.log("milse");
+        window.location.href="./home.html"
         
     }
     else{
