841c8988a56c3c9af786ed603479d647bfd22a04
barakahtrust <barakahtrustbd@gmail.com>
2025-06-22 12:30:06 +0600

    aa

---
+++ js/home.js
@@ -0,0 +1,14 @@
+let inputAmount = document.getElementById ("inputAmount")
+let addMoneyPin = document.getElementById ("addMoneyPin")
+let addMoneyBtn = document.getElementById ("addMoneyBtn")
+
+addMoneyBtn.addEventListener("click", ()=> {
+    if (addMoneyPin.value == 2222) {
+        
+    }
+    else{
+        alert("pin vul")
+    }
+})
+
+====
